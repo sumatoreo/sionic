5cd2988872d51ff3fba4e1edf5bfc3029053f2be
sumatoreo <iskander.kamaliev@gmail.com>
2020-06-10 06:21:42 +0300

    Debug Bar

--- code/config/app.php
+++ code/config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Debug Bar
+         */
+        Barryvdh\Debugbar\ServiceProvider::class
     ],
 
     /*
@@ -227,6 +231,10 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        /*
+         * Debug Bar
+         */
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
